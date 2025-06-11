e019dc06b6e57cb0416086fc2bf943bc255a328f
Juan Eduardo Montes Figueroa <u202210775@upc.edu.pe>
2025-06-11 12:43:31 -0500

    first commit

---
+++ src/main/java/com/bonos/BonosApplication.java
@@ -0,0 +1,13 @@
+package com.bonos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonosApplication.class, args);
+    }
+
+}
